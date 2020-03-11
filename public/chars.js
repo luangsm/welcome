fb30d640417ce635881dd23e82b6981c45f32bff
luangsm <60980922+luangsm@users.noreply.github.com>
2020-03-11 16:28:21 -0300

    Update chars.js

--- public/chars.js
+++ public/chars.js
@@ -1,43 +1,54 @@
 export default function charset() {
 
-		const player = {
-				atk: 10,
-				def: 7,
-				hp: 20,
-				ini: 14
-		}
+    const state = {
+        observers: []
+    }
 
-		const enemy = {
-				atk: 8,
-				def: 8,
-				hp: 15,
-				ini: 10
-		}
+    function subscribe(observerFunction) {
+        state.observers.push(observerFunction)
+    }
 
-		// Inicia as variaves do player e inimigo que ira ter seus stats mudados on demand
-		var stat = {}
-		var stats = {}
-		stats.hp = [player.hp]
-		stats.ini = [player.ini]
-		stat.hp = [enemy.hp]
-		stat.ini = [enemy.ini]
+    function notifyAll(command) {
+        for (const observerFunction of state.observers) {
+            observerFunction(command)
+        }
+    }
+
+        // Inicia as variaves do player e inimigo que ira ter seus stats mudados on demand
+        var info = {
+            stat: {},
+            stats: {},
+            coin: []
+        }
+		
+
+        function setbattle(chars) {
+            info.stats.hp = [chars.player.hp]
+            info.stats.ini = [chars.player.ini]
+            info.stats.atk = [chars.player.atk]
+            info.stats.def = [chars.player.def]
+            console.log(`Player Iniciado com ${info.stats.hp} de vida, ${info.stats.ini} de iniciativa, ${info.stats.atk} de ataque e ${info.stats.def} de defesa`)
+            info.stat.hp = [chars.enemy.hp]
+            info.stat.ini = [chars.enemy.ini]
+            info.stat.atk = [chars.enemy.atk]
+            info.stat.def = [chars.enemy.def]
+            console.log(`Inimigo Iniciado com ${info.stat.hp} de vida, ${info.stat.ini} de iniciativa, ${info.stat.atk} de ataque e ${info.stat.def} de defesa`)
+            if (info.stats.ini > info.stat.ini) {
+                info.coin = 0
+                notifyAll(info)
+            }
+            else {
+                info.coin = 1
+                notifyAll(info)
+            }
+            
+        }
 
 
-		function setact(act) {
-			if (act == "atk") {
-				stats.act = [act]
-				stats.atk = [player.atk]
-				console.log(`Ação ${act}, causa ${stats.atk} de dano`)
-			}
-			else{
-				stats.act = [act]
-				stats.def = [player.def]
-				console.log(`Ação ${act}, defende ${stats.def} de dano`)
-			}
-		}
 
 
 		return{
-			setact
+            setbattle,
+            subscribe
 		}
 }
